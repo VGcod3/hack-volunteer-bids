5c761504ec25f1ffe196c8b68ef1aca39e09593c
rosmorkl <rostyslavmorwork@gmail.com>
2024-02-08 02:34:08 +0200

    setup docker for api and docker compose

--- API/Bids/Bids.Api/Controllers/AuctionsController.cs
+++ API/Bids/Bids.Api/Controllers/AuctionsController.cs
@@ -1,71 +1,77 @@
+using Bids.Core.Entities.Auctions;
+using Bids.Core.Entities.Bids;
 using Bids.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bids.Api.Controllers;
 
 [ApiController]
-public class AuctionController : Controller
+[Route("/api/[controller]")]
+public class AuctionsController : Controller
 {
-    public AuctionController()
+    public AuctionsController()
     {
         
     }
 
-    [HttpPost("auctions/add")]
+    [HttpPost("add")]
     public IActionResult Create([FromBody] Auction auction)
     {
         return Ok();
     }
     
-    [HttpPost("auctions/{auctionId:long}/bids/add")]
+    [HttpPost("{auctionId:long}/bids/add")]
     public IActionResult Create([FromRoute] long auctionId, [FromBody] Bid auction)
     {
         return Ok();
     }
     
-    [HttpDelete("auctions/{auctionId:long}")]
+    [HttpDelete("{auctionId:long}")]
     public IActionResult Delete([FromRoute] long auctionId)
     {
         return Ok();
     }
     
-    [HttpDelete("auctions/{auctionId:long}/bids/{bidId:guid}")]
+    [HttpDelete("{auctionId:long}/bids/{bidId:guid}")]
     public IActionResult Delete([FromRoute] long auctionId, [FromRoute] Guid bidId)
     {
         return Ok();
     }
     
-    [HttpPost("auctions")]
+    [HttpPost]
     public IActionResult Find([FromBody] IAuctionFilter filter)
     {
         return Ok();
     }
     
-    [HttpPost("auctions/{auctionId:long}/bids")]
+    [HttpPost("{auctionId:long}/bids")]
     public IActionResult Find([FromRoute] long auctionId, [FromBody] IBidFilter filter)
     {
         return Ok();
     }
     
-    [HttpGet("auctions/{auctionId:long}")]
+    [HttpGet("{auctionId:long}")]
     public IActionResult Get([FromRoute] long auctionId)
     {
-        return Ok();
+        return Ok(new
+        {
+            hello = "hello focker"
+        });
     }
     
-    [HttpGet("auctions/{auctionId:long}/bids/{bidId:guid}")]
+    [HttpGet("{auctionId:long}/bids/{bidId:guid}")]
     public IActionResult Get([FromRoute] long auctionId, [FromRoute] Guid bidId)
     {
         return Ok();
     }
     
-    [HttpPut("auctions/{auctionId:long}")]
+    [HttpPut("{auctionId:long}")]
     public IActionResult Update([FromRoute] long auctionId, [FromBody] Auction auction)
     {
         return Ok();
     }
     
-    [HttpPut("auctions/{auctionId:long}/bids/{bidId:guid}")]
+    [HttpPut("{auctionId:long}/bids/{bidId:guid}")]
     public IActionResult Update([FromRoute] long auctionId, [FromRoute] Guid bidId, [FromBody] Bid bid)
     {
         return Ok();
